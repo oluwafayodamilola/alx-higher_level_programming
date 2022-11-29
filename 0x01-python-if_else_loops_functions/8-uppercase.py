b3e690578b7e0d64747a8ec2392d5f98758eae10
adeolaakee <adeolaakee@gmail.com>
2022-11-29 19:57:05 +0000

    power function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for i in str:
         asc = ord(i)
         convert = asc - 32 if (97 <= asc <= 122) else asc
-        print("{:c}".format(covert), end=" ")
+        print("{:c}".format(covert), end="")
