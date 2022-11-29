bac939c437660632c7fdcf268f2ee30a5c80a8a4
adeolaakee <adeolaakee@gmail.com>
2022-11-29 19:50:01 +0000

    power function

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    return True:
+        if (97 <= ord(c) <= 122)
+        else:
+            False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        asc = ord(i)
+        convert = asc - 32 if (97 <= 122) else asc
+        print("{:c}".format(covert), end=" ")
