6586f45af871f12ec84e2a0d52e53c8ecdb7f9c2
adeolaakee <adeolaakee@gmail.com>
2022-12-06 09:18:57 +0000

    returning an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """
+    Retrieves an element from a list
+
+    Args:
+        my_list: list to retrieve element from
+        idx: index to retrieve element
+
+    Return:
+        element at index or None if not found
+    """
     if idx < 0:
         return None
     if idx >= len(my_list):
