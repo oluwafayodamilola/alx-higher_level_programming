cbca374ae298d0600fb1e3eefdb82227dfe419c1
adeolaakee <adeolaakee@gmail.com>
2022-12-06 09:20:10 +0000

    returning an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -14,6 +14,6 @@ def element_at(my_list, idx):
         return None
     if idx >= len(my_list):
         return None
-    for index, element in my_list:
+    for index, element in enumerate(my_list):
         if index == idx:
             return element
