b56718620d1352e0975c66a551813e56fa8a8c9b
adeolaakee <adeolaakee@gmail.com>
2022-12-06 09:16:18 +0000

    returning an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return(None)
-    if idx > len(my_list):
-        return(None)
-    for i in my_list:
-        if (my_list.index(i) == idx)
-        return(i)
+        return None
+    if idx >= len(my_list):
+        return None
+    for index, element in my_list:
+        if index == idx:
+            return element
