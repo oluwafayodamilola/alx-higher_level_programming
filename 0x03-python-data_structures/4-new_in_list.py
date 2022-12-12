4e86ebae9ceb58fa1cfadc7ae643543bea2401a8
adeolaakee <adeolaakee@gmail.com>
2022-12-12 13:12:11 +0000

    replacing an a value at an index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,10 +4,11 @@ def new_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
+
     new_list = []
     for index, item in enumerate(my_list):
         if index == idx:
             new_list.append(element)
             continue
         new_list.append(item)
-        return new_list
+    return new_list
