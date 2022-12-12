b0b4232c6800a73b828fbab711300c2b3c4c1ef8
adeolaakee <adeolaakee@gmail.com>
2022-12-12 10:37:34 +0000

    replacing an a vuleu at an index

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+def new_inlist(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    new_list = []
+    for index, items in enumerate)my_list):
+        if index == idx:
+            new_list.append(element)
+            continue
+        new_list.append(item)
+        return new_list
